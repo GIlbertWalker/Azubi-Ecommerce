7610864db7d91ddfa6fe96fa644745d9449bf64c
dennis.muthuri <denmurithi@gmail.com>
2023-03-20 23:57:08 +0300

    git

---
+++ src/components/Navbarlogin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Navbarlogin = () => {
+  return (
+    <div className='main'>
+    <li>AzubiShop</li>
+    <div className='right-cont'>
+    Already have an account?
+    <li><a href='login'><button className='btn'>Login</button></a></li>
+    </div>
+    </div>
+  )
+}
+
+export default Navbarlogin
