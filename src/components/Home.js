de5fccfea4ce5e61740f3ab1544cefa2ea865196
Gilbert Ofori-Boye <gilbertoforiboye@gmail.com>
2023-03-28 16:30:54 +0000

    images corection

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,17 +7,22 @@ export default function CartPage() {
   return (
     <div>
     <div className="navbar">
-        <NavbaRegister Ninfo="New to AzubiShop?" btnName="Sign Up"/>
+        <NavbaRegister 
+        Cart="Cart"
+        Home="Home"
+        btnName="Login"/>
     </div>
     <div className="product-container">
       {allcarts.map((product, index) => (
         <div className="product-card" key={index}>
-          <img src={product.img} alt={product.product} />
-          <h2>{product.product}</h2>
-          <p>{product.productDescription}</p>
-          <div className='priceTag'>
-          <button>Place order</button>      
-          <p>{product.price}</p>
+          <div className='card-items'>
+            <img src={product.img} alt={product.product} />
+            <h2>{product.product}</h2>
+            <p className='com_info'>{product.productDescription}</p>
+            <div className='priceTag'>
+              <button className='order'>Place order</button>      
+            <p>{product.price}</p>
+            </div>
           </div>
         </div>
       ))}
