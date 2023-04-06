610976eef3b0e90150352c6ecd6cb61a77ea8fab
Gilbert Ofori-Boye <gilbertoforiboye@gmail.com>
2023-04-06 02:07:10 +0000

    comiting changes to hompage

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import { ReactComponent as SearchIcon } from './search.svg';
+// import React from 'react';
+// import { ReactComponent as SearchIcon } from './search.svg';
 import '../Css/SearchBar.css';
 
-function SearchBar() {
+// function SearchBar() {
+
+  
+//   return (
+//     <div className="search-bar">
+//       <div class="input-group mb-3">
+//           <div class="input-group-prepend">
+//             <span class="input-group-text" id="basic-addon1"><SearchIcon className='search-icon'/></span>
+//           </div>
+//           <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+//         </div>
+//     </div>
+//   );
+// }
+
+// export default SearchBar;
+
+
+import { useState } from 'react';
+
+export default function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchInputEnter = (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      onSearch(searchTerm.trim());
+    }
+  };
+
   return (
-    <div className="search-bar">
-        <SearchIcon className="search-icon" />
-        <input type="text" placeholder="Search product" />
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search product"
+        value={searchTerm}
+        onChange={handleSearchInputChange}
+        onKeyDown={handleSearchInputEnter}
+      />
+      <button type="submit">
+        <img src="/search-icon.png" alt="search icon" />
+      </button>
+
     </div>
   );
 }
-
-export default SearchBar;
